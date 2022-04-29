e66ceb59bd3c682809614d57eb90e44bcb6f5e61
SanSheng9 <sansheng9@yadnex.ru>
2022-04-29 23:21:40 +1000

    start project

---
+++ shop/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from shop.models import Product
+from shop.serializers import ProductSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
